55d12ad85e4dcc1166e19b28765b35cfbf720eb6
hanna <hanna.hummal@khk.ee>
2022-11-05 21:50:58 +0200

    as an user I can see the products

--- src/App.js
+++ src/App.js
@@ -4,34 +4,6 @@ import { useState, useEffect } from "react";
 import React from 'react';
 //import Products from "./components/addProducts";
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       items: [],
-//       isLoaded: false
-//     }
-//   }
-
-//   componentDidMount() {
-//     fetch('https://fakestoreapi.com/products')
-//       .then(res => res.json())
-//       .then(json => {
-//         this.setState({
-//           items: json,
-//           isLoaded: true,
-//         })
-//       }).catch((err) => {
-//         console.log(err);
-//       });
-//   }
-
-//   render() {
-//     const { isLoaded, items } = this.state;
-//     if (!isLoaded)
-//       return <div>Loading...</div>;
-
-
 const App = () => {
 
   const [items, setItems] = useState([]);
